7dd09e33e5444784286d1bdca05bc59bb00b293f
Lucas Lopes <lucaslopesoliveira@gmail.com>
2023-02-16 22:21:18 -0300

    melhoria no maps

--- interno/Map.js
+++ interno/Map.js
@@ -1,54 +1,70 @@
-import { SafeAreaView, StyleSheet} from "react-native";
-import MapView, {Marker, Polyline} from "react-native-maps";
+import { SafeAreaView, StyleSheet, View } from "react-native";
+import MapView, { Marker, Polyline } from "react-native-maps";
 import { useState } from "react";
-import MapViewDirections from 'react-native-maps-directions';
-import * as Location from 'expo-location';
+import MapViewDirections from "react-native-maps-directions";
+import * as Location from "expo-location";
 import dados from "../dados/pontos.json";
 import dados2 from "../dados/areas.json";
+import { Button, Card, IconButton, List } from "react-native-paper";
+import { set } from "react-hook-form";
 
 export default function Map({ navigation }) {
-  const [regiao, setRegiao]= useState({
-    latitude:-3.731862,
-    longitude:-38.526669,
+  const [regiao, setRegiao] = useState({
+    latitude: -3.731862,
+    longitude: -38.526669,
     latitudeDelta: 0.0922,
-    longitudeDelta: 0.0421
+    longitudeDelta: 0.0421,
   });
   const [origem, setOrigem] = useState();
   const [destino, setDestino] = useState();
+  const [card, setCard] = useState(false);
+  const [styleMap, setStyleMap] = useState({
+    width: "100%",
+    height: "100%",
+  });
 
-  const userLocation = async()=>{
+  const userLocation = async () => {
     let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied');
-        return;
-      }
+    if (status !== "granted") {
+      setErrorMsg("Permission to access location was denied");
+      return;
+    }
 
-      let location = await Location.getCurrentPositionAsync({enableHighAccuracy: true});
-      setOrigem({
-        latitude: location.coords.latitude,
-        longitude: location.coords.longitude,
-      });
+    let location = await Location.getCurrentPositionAsync({
+      enableHighAccuracy: true,
+    });
+    setOrigem({
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude,
+    });
+  };
 
-  }
-
-  function rota(destino){
+  function novaRota(){
     userLocation();
+  }
+  
+  function rota(destino) {
+    setCard(true);
     setDestino(destino.coordinate);
+    setStyleMap({
+      width: "100%",
+      height: "80%",
+    })
   }
 
-  //const GOOGLE_MAPS_APIKEY = 'AIzaSyDBscTlHcyJ5FTZ
-  //5Dlw7-ojc3LPhFfbAuY';
+  //const GOOGLE_MAPS_APIKEY = "AIzaSyDBscTlHcyJ5F
+  //TZ5Dlw7-ojc3LPhFfbAuY";
 
   function renderMarkers() {
     return dados.map((x, i) => (
       <Marker
-        image={require('../assets/gps.png')}
+        image={require("../assets/gps.png")}
         key={i}
         coordinate={{
           latitude: x.latitude,
-          longitude: x.longitude 
+          longitude: x.longitude,
         }}
-        onPress={e => rota(e.nativeEvent)}
+        onPress={(e) => rota(e.nativeEvent)}
       />
     ));
   }
@@ -56,7 +72,11 @@ export default function Map({ navigation }) {
   function renderAreas() {
     return dados2.map((d) => {
       return (
-        <Polyline coordinates={d.path} strokeColor="#027373" strokeWeight={10} />
+        <Polyline
+          coordinates={d.path}
+          strokeColor="#027373"
+          strokeWeight={10}
+        />
       );
     });
   }
@@ -65,13 +85,26 @@ export default function Map({ navigation }) {
     <SafeAreaView style={styles.container}>
       <MapView
         region={regiao}
-        style={styles.map}
+        style={styleMap}
         showsUserLocation={true}
-        showsMyLocationButton={true}>
-
+        showsMyLocationButton={true}
+        onPress={(e) => {
+          if (e.nativeEvent.action !== 'marker-press') {
+            setOrigem(null);
+            setCard(false);
+            // pressed a marker
+            setStyleMap({
+              width: "100%",
+              height: "100%",
+            })
+          } else {
+            
+          }
+        }}
+      >
         {renderMarkers()}
         {renderAreas()}
-        
+
         <MapViewDirections
           strokeWidth={3}
           strokeColor="#027373"
@@ -79,24 +112,41 @@ export default function Map({ navigation }) {
           destination={destino}
           apikey={GOOGLE_MAPS_APIKEY}
         />
-
       </MapView>
+      {
+        card ?
+                <Card style={{
+                  height:"20%"
+                }}>
+                  <Card.Content>
+                    <List.Item
+                      title="Vaga 1"
+                      description="Regra vaga ...."
+                      left={() => <IconButton icon="car" size={30} />}
+                      right={() => 
+                              <View>
+                                <Button onPress={() => novaRota()}>Rota</Button>
+                                <Button mode="contained">Estacionar</Button>
+                              </View>
+                      }
+                    />
+                  </Card.Content>
+                </Card>
+              : 
+                null
+      }
+      
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
-  },
-  button: {
-    position: 'absolute',
-    top: '90%',
-    left: '30%', 
-    alignSelf: 'flex-end' 
+    flex: 1,
+    position: "relative"
   },
   map: {
     width: "100%",
-    height: "100%",
+    height: "80%",
   },
 });
